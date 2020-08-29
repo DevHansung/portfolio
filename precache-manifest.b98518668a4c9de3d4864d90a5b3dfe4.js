84e71c62349454facb989f4029e7cca0a16099b3
DevHansung <dlgkstjd23@naver.com>
2020-08-29 16:13:27 +0900

    Updates

---
+++ precache-manifest.b98518668a4c9de3d4864d90a5b3dfe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e81c9f36ebc1d091d0cd",
+    "url": "/portfolio/static/css/main.6393eab6.chunk.css"
+  },
+  {
+    "revision": "e81c9f36ebc1d091d0cd",
+    "url": "/portfolio/static/js/main.e81c9f36.chunk.js"
+  },
+  {
+    "revision": "d952503c076df2ccee9a",
+    "url": "/portfolio/static/css/1.90a898db.chunk.css"
+  },
+  {
+    "revision": "d952503c076df2ccee9a",
+    "url": "/portfolio/static/js/1.d952503c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "42a6d3d3a29944d3087eba595ece2c99",
+    "url": "/portfolio/index.html"
+  }
+];
